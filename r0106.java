1efcfee1d4fa614c5829e5b6230852ef253735ff
Alejandro Tolaba <alejandrotolaba@gmail.com>
2023-04-06 21:08:01 -0300

    Resuelto el ejercicio 06 de la unidad 01 (r0106)

--- r0106.java
+++ r0106.java
@@ -9,6 +9,19 @@ import java.util.Scanner;
 
 public class r0106 {
 	public static void main(String args[]) {
-		// Codigo
+		Scanner scan = new Scanner(System.in);
+		int nota1, nota2, nota3;
+		int prom;
+
+		System.out.print("Ingrese la primera nota: ");
+		nota1 = scan.nextInt();
+		System.out.print("Ingrese la segunda nota: ");
+		nota2 = scan.nextInt();
+		System.out.print("Ingrese la tercera nota: ");
+		nota3 = scan.nextInt();
+
+		prom = ((nota1 + nota2 + nota3) / 3);
+
+		System.out.println("El promedio es: " + prom);
 	}
 }
