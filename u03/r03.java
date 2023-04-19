c0c3399b2ec94ec20159f86a4c33fd24a42ea211
Alejandro Tolaba <alejandrotolaba@gmail.com>
2023-04-19 03:51:12 -0300

    Resuelto el ejercicio de ejemplo 03 de la unidad 03 (r0303)

---
+++ u03/r03.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+
+/*
+ *  Realizar el juego «el numero secreto», que consiste en acertar un número
+ *  desconocido (generado aleatoriamente entre 1 y 100). Para ello se leen por
+ *  teclado una serie de números, para los que se indica: «mayor» o «menor»,
+ *  según sea mayor o menor con respecto al número secreto. El proceso termina
+ *  cuando el usuario acierta o cuando se rinde (introduciendo un -1)
+ */
+
+
+public class r03 {
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+
+		int numSecreto = (int) (Math.random() * 100 + 1); //número aleatorio entre 1 y 100
+		int num = 0; // Inicializamos con 0 por que no puede ser el número secreto
+
+		while (num != numSecreto) {
+			System.out.print("Adivine el número secreto (-1 para salir): ");
+			num = scan.nextInt();
+
+			if (num == -1) {break;} // Salir si num = -1
+
+			if (num > numSecreto) {
+				// Si el número elegido es mayor al secreto
+				System.out.println(" - Tu número es mayor al numero secreto");
+			} else if (num < numSecreto) {
+				// Si el número elegido es menor al secreto
+				System.out.println(" - Tu número es menor al número secreto");
+			} else {
+				// Si el número elegido es igual al secreto
+				System.out.println(" - Adivinaste");
+			}
+			System.out.println();
+		}
+	}
+}
